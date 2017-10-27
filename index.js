b8b780346bd6da26ff510f52cdc1320a3d55ef90
Connor Giles <cgiles6@uwo.ca>
2017-10-27 16:23:10 -0400

    Added procfile

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 var express = require('express')
 var app = express()
 const bodyParser = require('body-parser');
+/*
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+*/
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
