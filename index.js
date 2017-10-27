53164ae8811bc307494d3b4c4d4d704df2582e66
Connor Giles <cgiles6@uwo.ca>
2017-10-27 16:28:21 -0400

    Change port

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ app.get('/db', function (request, response) {
 });
 */
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -32,6 +34,6 @@ app.post('*', function (req, res) {
   res.send('hello world')
 })
 
-app.listen(80, function () {
+app.listen(app.get('port'), function () {
   console.log('Example app listening on port 3000!')
 })
