3e051d968823f2eade5932586b7a0db3cdbc03d4
Connor Giles <cgiles6@uwo.ca>
2017-10-27 16:52:50 -0400

    fixed postgres?

--- index.js
+++ index.js
@@ -4,21 +4,6 @@ const pg = require('pg')
 const format = require('pg-format')
 
 var app = express()
-/*
-var pg = require('pg');
-
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-});
-*/
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -34,25 +19,39 @@ app.get('*', function (req, res) {
 
 app.post('/strokes', function (req, res) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-
     const insertText = format('INSERT INTO strokes(user, time, keys, modifiers, direction) VALUES %L', req.body.map(r => {
       return [r.user, r.time, r.keyCode, r.modifiers, r.direction]
     }))
     console.log(insertText)
-    await client.query(insertText)
-    res.send('Success')
-    done()
+
+    client.query(insertText, (err, res) => {
+      if (err) {
+        console.log(err.stack)
+        res.send(err.stack)
+        done()
+      } else {
+        res.send('success')
+        done()
+      }
+    })
   })
 })
 
 app.get('/strokes', function (req, res) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    const result = await client.query('SELECT * FROM strokes LIMIT ' + (req.query.limit || 20))
-    res.send(result.rows)
-    done()
+    client.query('SELECT * FROM strokes LIMIT ' + (req.query.limit || 20), (err, res) => {
+      if (err) {
+        console.log(err.stack)
+        res.send(err.stack)
+        done()
+      } else {
+        res.send(res.rows)
+        done()
+      }
+    })
   })
 })
 
 app.listen(app.get('port'), function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port ' + app.get('port'))
 })
