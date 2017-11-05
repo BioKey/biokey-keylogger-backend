a0de843e0b14154c17c2c229b4a95b35b41a98c4
Connor Giles <cgiles6@uwo.ca>
2017-11-04 23:33:59 -0400

    Changed file limit size

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('*', function (req, res) {
